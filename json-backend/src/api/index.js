2885ab27a38110f5973e0814dc83b08d88262939
Jorge Saldivar <49691994+Jorge0611@users.noreply.github.com>
2024-02-14 19:14:16 -0400

    use json server as middleware for express

--- json-backend/src/api/index.js
+++ json-backend/src/api/index.js
@@ -2,8 +2,10 @@ const brand = require("./brand.json");
 const sneakers = require("./sneakers.json");
 const sizes = require("./sizes.json");
 
-module.exports = () => ({
-  brand: brand,
-  sneakers: sneakers,
-  sizes: sizes,
-});
+const data = {
+  brand,
+  sneakers,
+  sizes,
+};
+
+module.exports = data;
